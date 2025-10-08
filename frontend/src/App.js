e07d85d999dda46ecc4db4142d44aac5093d05eb
Hugo Moslener <hugo.moslener@gmail.com>
2025-10-08 12:08:28 -0400

    Added a proxy that react can use to interact with the backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,5 @@
 import './App.css';
 //import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import HealthCheck from './HealthCheck';
 export default function App() {
-    return (
-        <div style={{padding: 24}}>
-            <h1>Proxy check</h1>
-            <HealthCheck/>
-        </div>
-    );
+    return;
 }
