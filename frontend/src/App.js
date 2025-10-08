6de8043efe341a63adfb5c6bfd99806666a674c4
Hugo Moslener <hugo.moslener@gmail.com>
2025-10-08 12:10:21 -0400

    Added a proxy that react can use to interact with the backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
 import './App.css';
-//import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-export default function App() {
-    return;
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+    return (
+        <Router>
+            <nav>
+                {/* Links to navigate without reloading */}
+                <Link to="/">Home</Link>
+                <Link to="/test"></Link>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
 }
+
+export default App;
