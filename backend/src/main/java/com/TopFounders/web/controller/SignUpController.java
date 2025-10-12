98e24529eba208a90660370c50e9448014252613
Alvin <bijualvin2@gmail.com>
2025-10-12 13:00:15 -0400

    Factory pattern

--- backend/src/main/java/com/TopFounders/web/controller/SignUpController.java
+++ backend/src/main/java/com/TopFounders/web/controller/SignUpController.java
@@ -3,6 +3,8 @@ package com.TopFounders.web.controller;
 import com.TopFounders.domain.model.Operator;
 import com.TopFounders.domain.model.Rider;
 import com.TopFounders.domain.model.User;
+import com.TopFounders.domain.model.UserFactory;
+import com.TopFounders.domain.service.OperatorService;
 import com.TopFounders.domain.service.RiderService;
 import com.TopFounders.domain.service.UserService;
 import com.google.api.client.json.Json;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 public class SignUpController {
 
     private final RiderService riderService = new RiderService();
+    private final OperatorService operatorService = new OperatorService();
 
     @GetMapping("/hello")
     public String getHello(){
@@ -26,7 +29,22 @@ public class SignUpController {
     public String saveData(@RequestBody Rrider rider ){
         try{
             System.out.println("Post request reached here");
-            String message = riderService.saveRider(new Rider(rider.getUsername(),rider.getPaymentInformation(),rider.getEmail(),rider.getFullName(),rider.getAddress(), "rider"));
+            UserFactory factory = new UserFactory();
+            String message = riderService.saveRider((Rider)factory.CreateUser(rider.getUsername(),rider.getPaymentInformation(),rider.getEmail(),rider.getFullName(),rider.getAddress(), "rider"));
+            return "true";
+        }
+        catch (Exception e) {
+            return "false";
+        }
+
+    }
+
+    @PostMapping("/saveOperator")
+    public String saveDataOperator(@RequestBody Ooperator operator ){
+        try{
+            System.out.println("Post request reached here");
+            UserFactory factory = new UserFactory();
+            String message = operatorService.saveOperator((Operator)factory.CreateUser(operator.getUsername(),"",operator.getEmail(),operator.getFullName(),operator.getAddress(), "operator"));
             return "true";
         }
         catch (Exception e) {
