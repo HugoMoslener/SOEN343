be76969bcb5a646a05d71e785ab812f149873998
Alvin <bijualvin2@gmail.com>
2025-10-17 22:32:40 -0400

    update

--- backend/src/main/java/com/TopFounders/ui/controller/OperatorHelperClass.java
+++ backend/src/main/java/com/TopFounders/ui/controller/OperatorHelperClass.java
@@ -1,23 +1,19 @@
 package com.TopFounders.ui.controller;
 
-public class RiderController {
+public class OperatorHelperClass {
     private String email;
     private String username;
-    private String paymentInformation;
     private String address;
     private String fullName;
-    
-    public RiderController(){}
-    
+
+    public OperatorHelperClass(){}
+
     public String getAddress(){return this.address;}
     public String getUsername(){return this.username;}
-    public String getPaymentInformation(){return this.paymentInformation;}
     public String getEmail(){return this.email;}
     public String getFullName(){return this.fullName;}
     public void setAddress(String address){ this.address=address;}
     public void setUsername(String username){this.username=username;}
-    public void setPaymentInformation(String paymentInformation){this.paymentInformation=paymentInformation;}
     public void setEmail(String email){this.email = email;}
     public void setFullName(String fullName){this.fullName=fullName;}
-
 }
