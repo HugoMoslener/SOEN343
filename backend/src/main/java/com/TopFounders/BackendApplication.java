21f1ccec4d443e5ffdfb816ca93d7a9573d0812e
Alvin <bijualvin2@gmail.com>
2025-10-17 20:47:24 -0400

    Implementing a scheduler to automatically check the reservation expiration time

--- backend/src/main/java/com/TopFounders/BackendApplication.java
+++ backend/src/main/java/com/TopFounders/BackendApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@RestController
+@EnableScheduling
 public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
